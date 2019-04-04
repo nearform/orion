ec4992745c79f985bff05328a769de6009867353
Artur Daschevici <a.daschevici@gmail.com>
2019-04-04 09:51:26 +0100

    Add snapshot testing for storybook (#17)
    * Add snapshot testing for storybook
    
    * Remove unnecessary environment variable
    
    * Update snapshots
    
    * Update snapshots

--- packages/components/.storybook/config.js
+++ packages/components/.storybook/config.js
@@ -19,6 +19,29 @@ addDecorator(withKnobs)
 addDecorator(withThemes(themes))
 setAddon(JSXAddon)
 
+if (process.env.NODE_ENV === 'test') {
+  const { getFullPath, requireModules } = require('./require-context')
+  /**
+   * If we are in the test environment, we need to polyfill require.context for
+   * CRA testing environment. It's no longer included in storyshots.
+   * But this is the code from storyshots 3.4.11.
+   */
+  require.context = (
+    directory,
+    useSubdirectories = false,
+    regExp = /^\.\//
+  ) => {
+    const fullPath = getFullPath(__dirname, directory)
+
+    const keys = {}
+    requireModules(keys, fullPath, '.', regExp, useSubdirectories)
+
+    const req = f => keys[f]
+    req.keys = () => Object.keys(keys)
+    return req
+  }
+}
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/)
 
