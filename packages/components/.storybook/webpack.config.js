95a632f12fea5cc267e7932dc02e2631fe724f56
Artur Daschevici <a.daschevici@gmail.com>
2019-04-23 13:30:30 +0100

    feat: 🎸 Add static deploy for storybook stories (#130)
    Storybook needs to be deployed statically so that the UI changes are
    readily available for designers to inspect
    
    ✅ Closes: #108

---
+++ packages/components/.storybook/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = async ({ config }) => {
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'fs': path.resolve(__dirname, 'fsMock.js')
+  }
+
+  return config
+}
