33179f4e47f88875bbaa5401d009ebe331f516ac
Glen Keane <glenkeane.94@gmail.com>
2019-04-26 10:08:49 +0100

    feat(Auth): Adds custom fields to the sign-up form to capture extra user information (#118)
    * feat(Auth): Adds custom fields to the sign-up form to capture extra user information
    
    Adds custom fields to the sign-up form to capture user org, orgType and country. Unfortunately we
    can't use select elements in amplify react so as a first pass I have stored country as a text
    element. The same applies to radio elements for the org-type (none, member, partner)
    
    closes #95
    
    * fix(config): Update changes to fit new layout
    
    Updates this to reflect changes in commit 7bbbc451d5627c75f410760d111fcbbdf7971d2c

--- packages/app/src/pages/auth/index.js
+++ packages/app/src/pages/auth/index.js
@@ -6,7 +6,57 @@ import DisplayIfSignedIn from '../../components/DisplayIfSignedIn'
 
 export default function Auth() {
   return (
-    <Authenticator hide={[Greetings]}>
+    <Authenticator
+      hide={[Greetings]}
+      signUpConfig={{
+        hideAllDefaults: true,
+        signUpFields: [
+          {
+            label: 'Email',
+            key: 'username',
+            required: true,
+            placeholder: 'Email',
+            type: 'email',
+            displayOrder: 1,
+          },
+          {
+            label: 'Password',
+            key: 'password',
+            required: true,
+            placeholder: 'Password',
+            type: 'password',
+            displayOrder: 2,
+          },
+          {
+            label: 'Type Of Organisation',
+            key: 'orgType',
+            required: true,
+            custom: true,
+            placeholder: 'Org Type',
+            type: 'text',
+            displayOrder: 3,
+          },
+          {
+            label: 'Name Of Organisation',
+            key: 'orgName',
+            required: true,
+            custom: true,
+            placeholder: 'Org Name',
+            type: 'text',
+            displayOrder: 4,
+          },
+          {
+            label: 'Country',
+            key: 'country',
+            required: true,
+            custom: true,
+            placeholder: 'Country',
+            type: 'text',
+            displayOrder: 5,
+          },
+        ],
+      }}
+    >
       <DisplayIfSignedIn>
         <Redirect to="/" noThrow />
       </DisplayIfSignedIn>
