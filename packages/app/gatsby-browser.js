77d66561301a74ffb2fb42753b06497c1503377c
Matija Gaspar <matija.gaspar@gmail.com>
2019-06-10 11:28:48 +0200

    fix(app): fix reddirect from /admin if authenticated

--- packages/app/gatsby-browser.js
+++ packages/app/gatsby-browser.js
@@ -7,22 +7,29 @@ import { assessments } from './theme.es'
 import Layout from './src/components/Layout'
 import addTranslations from './utils/translations'
 import RootWrapper from './src/components/RootWrapper'
-
+import { useState, useEffect } from 'react'
 const client = new GraphQLClient({
   url: process.env.GATSBY_GRAPHQL_API,
 })
 
-export async function onClientEntry() {
-  const i18next = await i18n.init()
-  addTranslations(assessments, i18next)
+const InitializedWrapper = ({ element }) => {
+  const [isInitialised, setIsInitialised] = useState(false)
+  useEffect(() => {
+    const init = async () => {
+      await auth.init(client)
+      const i18next = await i18n.init()
+      addTranslations(assessments, i18next)
+      setIsInitialised(true)
+    }
+    init()
+  }, [])
+  return isInitialised ? (
+    <RootWrapper client={client}>{element}</RootWrapper>
+  ) : null
 }
-export async function onInitialClientRender() {
-  await auth.init(client)
+export const wrapRootElement = ({ element }) => {
+  return <InitializedWrapper element={element} />
 }
-
-export const wrapRootElement = ({ element }) => (
-  <RootWrapper client={client}>{element}</RootWrapper>
-)
 
 export const wrapPageElement = ({ element, props }) => (
   <Layout darkToolbar={props.location.pathname === '/'}>{element}</Layout>
