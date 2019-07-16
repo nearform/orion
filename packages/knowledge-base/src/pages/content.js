4944b8d8aba5aa4312f9b304c900e00ca7f36bdb
Kenneth Ahlstrom <ken@aeonvox.com>
2019-07-16 09:17:17 -0600

    Basic Routing

---
+++ packages/knowledge-base/src/pages/content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Router } from '@reach/router'
+
+import ProtectedRoute from '../components/ProtectedRoute'
+import ViewArticle from '../components/ViewArticle'
+
+export default function Content() {
+  return (
+    <Router basepath="/content">
+      <ProtectedRoute component={ViewArticle} path="*" />
+    </Router>
+  )
+}
