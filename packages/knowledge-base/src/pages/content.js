bdeadc9962f2e3929820ba20c05cd4b23dd9059e
Ken Ahlstrom <ken@aeonvox.com>
2019-07-29 18:06:21 +0300

    Forgot to return content.js back to normal state

--- packages/knowledge-base/src/pages/content.js
+++ packages/knowledge-base/src/pages/content.js
@@ -2,19 +2,11 @@ import React from 'react'
 import { Router } from '@reach/router'
 
 import ViewArticle from '../components/content/ViewArticle'
-import ProtectedRoute from '../components/ProtectedRoute'
 
 export default function Content() {
   return (
-    /*<Router>
-      <ViewArticle path="/content/:slug" />
-    </Router>*/
     <Router>
-      <ProtectedRoute
-        requiresGroup
-        component={ViewArticle}
-        path="/content/:slug"
-      />
+      <ViewArticle path="/content/:slug" />
     </Router>
   )
 }
