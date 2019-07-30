68d87f4bbe4c467bddb097a9faad5edfb7c2462e
Ken Ahlstrom <ken@aeonvox.com>
2019-07-30 14:56:06 +0300

    Forgot to remove my routing hack that fixes the JWT issue locally

--- packages/knowledge-base/src/pages/content.js
+++ packages/knowledge-base/src/pages/content.js
@@ -2,16 +2,10 @@ import React from 'react'
 import { Router } from '@reach/router'
 
 import ViewArticle from '../components/content/ViewArticle'
-import ProtectedRoute from '../components/ProtectedRoute'
 
 export default function Content() {
   return (
     <Router>
-      <ProtectedRoute
-        requiresGroup
-        component={ViewArticle}
-        path="/content/:slug"
-      />
       <ViewArticle path="/content/:slug" />
     </Router>
   )
