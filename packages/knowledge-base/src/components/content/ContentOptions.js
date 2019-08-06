7a86fc61dabd560f97cb24201b37898dbd7fc955
AlanSl <alan.slater@nearform.com>
2019-08-06 11:50:14 +0100

    chore(components): Only let platform admins hide/show articles

--- packages/knowledge-base/src/components/content/ContentOptions.js
+++ packages/knowledge-base/src/components/content/ContentOptions.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import T from 'prop-types'
 import { withStyles, Typography } from '@material-ui/core'
+import { useIsAdmin, useIsPlatformGroup } from '../../utils/auth'
 
 import HideButton from './HideButton'
 
 const ContentOptions = ({ classes, articleDetails }) => {
-  const canHide = true // TODO
+  const isAdmin = useIsAdmin()
+  const isPlatformGroup = useIsPlatformGroup()
+
+  const canHideArticles = isAdmin && isPlatformGroup
 
   return (
     <div className={classes.wrapper}>
@@ -13,7 +17,7 @@ const ContentOptions = ({ classes, articleDetails }) => {
       <Typography variant="h4">Print this page</Typography>
       <Typography variant="h4">Share this article</Typography>
       <Typography variant="h4">Rate this article</Typography>
-      {canHide && (
+      {canHideArticles && (
         <HideButton
           status={articleDetails.status}
           articleId={articleDetails.id}
