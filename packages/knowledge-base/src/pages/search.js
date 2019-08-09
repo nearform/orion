cd591f51fc7ae709a09b77f450283e327849a3e0
Ken Ahlstrom <ken@aeonvox.com>
2019-08-09 06:59:04 +0300

    Fix routes on page files

--- packages/knowledge-base/src/pages/search.js
+++ packages/knowledge-base/src/pages/search.js
@@ -2,16 +2,10 @@ import React from 'react'
 import { Router } from '@reach/router'
 
 import ListContent from '../components/content/ListContent'
-import ProtectedRoute from '../components/ProtectedRoute'
 
 export default function Content() {
   return (
     <Router>
-      <ProtectedRoute
-        requiresGroup
-        component={ListContent}
-        path="/search/:term"
-      />
       <ListContent path="/search/:term" />
     </Router>
   )
--- packages/knowledge-base/src/pages/section.js
+++ packages/knowledge-base/src/pages/section.js
@@ -2,16 +2,10 @@ import React from 'react'
 import { Router } from '@reach/router'
 
 import ListContent from '../components/content/ListContent'
-import ProtectedRoute from '../components/ProtectedRoute'
 
 export default function Content() {
   return (
     <Router>
-      <ProtectedRoute
-        requiresGroup
-        component={ListContent}
-        path="/section/:cat"
-      />
       <ListContent path="/section/:cat" />
     </Router>
   )
