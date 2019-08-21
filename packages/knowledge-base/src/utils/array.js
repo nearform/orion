6798dcb47ab6f1172158aeb7779994a57670d888
Ken Ahlstrom <ken@aeonvox.com>
2019-08-21 13:57:36 +0200

    Added safety to util function for when requested results is >= total possible results

--- packages/knowledge-base/src/utils/array.js
+++ packages/knowledge-base/src/utils/array.js
@@ -1,10 +1,14 @@
 export const getRandomRows = (source, num) => {
   const results = []
   const origin = source
-  while (results.length < num) {
-    const i = Math.floor(Math.random() * origin.length)
-    results.push(origin[i])
-    origin.splice(i, 1)
+  if (origin.length <= num) {
+    results.push(...origin)
+  } else {
+    while (results.length < num) {
+      const i = Math.floor(Math.random() * origin.length)
+      results.push(origin[i])
+      origin.splice(i, 1)
+    }
   }
 
   return results
