a74d28bd1dc28773fcfc79d7781df30099c965b8
Petr Kohut <me@petrkohut.cz>
2019-10-02 17:38:57 +0200

    add pino logger on lambda functions (#652)

--- packages/functions/src/jwt-enrichment-hook/index.js
+++ packages/functions/src/jwt-enrichment-hook/index.js
@@ -7,55 +7,62 @@ import {
 import { getUserGroup } from './user-groups'
 import getUserByCognitoId from './graphql/get-user-by-cognito-id.graphql'
 import { getCustomClaims } from './custom-claims'
+import pino from 'pino'
+const logger = pino()
 
 export const handler = async event => {
-  const cognitoId = event.request.userAttributes.sub
+  try {
+    const cognitoId = event.request.userAttributes.sub
 
-  let { user } = await graphql(getUserByCognitoId, {
-    cognitoId,
-  })
+    let { user } = await graphql(getUserByCognitoId, {
+      cognitoId,
+    })
 
-  if (!user.length) {
-    // TODO: user not found, will need to handle in some way
+    if (!user.length) {
+      // TODO: user not found, will need to handle in some way
+      return event
+    }
+
+    user = user[0]
+
+    const userGroup = getUserGroup(user)
+
+    if (userGroup) {
+      const userRoles = getUserRoles(user)
+      const defaultRoleName = selectDefaultRoleName(userRoles, userGroup)
+
+      event.response = {
+        claimsOverrideDetails: {
+          claimsToAddOrOverride: {
+            'https://hasura.io/jwt/claims': JSON.stringify({
+              'x-hasura-allowed-roles': [defaultRoleName],
+              'x-hasura-default-role': defaultRoleName,
+              'x-hasura-user-id': user.id.toString(),
+              'x-hasura-group-id': userGroup.id.toString(),
+            }),
+            ...getCustomClaims(user),
+          },
+        },
+      }
+    } else {
+      logger.info(`user ${cognitoId} doesn't belong to any groups`)
+
+      event.response = {
+        claimsOverrideDetails: {
+          claimsToAddOrOverride: {
+            'https://hasura.io/jwt/claims': JSON.stringify({
+              'x-hasura-allowed-roles': [DEFAULT_ROLE_NAME],
+              'x-hasura-default-role': DEFAULT_ROLE_NAME,
+              'x-hasura-user-id': user.id.toString(),
+            }),
+          },
+        },
+      }
+    }
+
     return event
+  } catch (e) {
+    logger.error(e)
+    throw e
   }
-
-  user = user[0]
-
-  const userGroup = getUserGroup(user)
-
-  if (userGroup) {
-    const userRoles = getUserRoles(user)
-    const defaultRoleName = selectDefaultRoleName(userRoles, userGroup)
-
-    event.response = {
-      claimsOverrideDetails: {
-        claimsToAddOrOverride: {
-          'https://hasura.io/jwt/claims': JSON.stringify({
-            'x-hasura-allowed-roles': [defaultRoleName],
-            'x-hasura-default-role': defaultRoleName,
-            'x-hasura-user-id': user.id.toString(),
-            'x-hasura-group-id': userGroup.id.toString(),
-          }),
-          ...getCustomClaims(user),
-        },
-      },
-    }
-  } else {
-    console.log(`user ${cognitoId} doesn't belong to any groups`)
-
-    event.response = {
-      claimsOverrideDetails: {
-        claimsToAddOrOverride: {
-          'https://hasura.io/jwt/claims': JSON.stringify({
-            'x-hasura-allowed-roles': [DEFAULT_ROLE_NAME],
-            'x-hasura-default-role': DEFAULT_ROLE_NAME,
-            'x-hasura-user-id': user.id.toString(),
-          }),
-        },
-      },
-    }
-  }
-
-  return event
 }
