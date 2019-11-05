f8479bad9d0ef507857c496c0586d1a9f9a19c5a
Julian Goacher <julian.goacher@innerfunction.com>
2019-11-05 15:31:59 +0000

    fix(#779): Use useAuthorizedQuery when reading user bookmarks.

--- packages/knowledge-base/src/hooks/useUserBookmarks.js
+++ packages/knowledge-base/src/hooks/useUserBookmarks.js
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-//import { useManualQuery } from 'graphql-hooks'
 import { getUserBookmarks } from '../queries'
 import { AuthContext, useAuthorizedQuery } from 'components'
 import get from 'lodash/get'
@@ -22,21 +21,6 @@ const useUserBookmarks = () => {
       onNoFetch: () => [],
     }
   )
-
-  /*
-  const [
-    fetchUserBookmarks,
-    { data: bookmarksData, loading: loadingBookmarks },
-  ] = useManualQuery(getUserBookmarks, {
-    variables: {
-      userId,
-    },
-  })
-
-  const userBookmarks = get(bookmarksData, 'user_bookmarks', []).map(
-    bookmark => bookmark.article_id
-  )
-  */
 
   return {
     userBookmarks,
