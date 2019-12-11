ee9958bf9e256cbb58536896abe3668dfb1bb7da
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2019-12-11 13:40:47 +0000

    feat(build): provide the ability to analyze the contents of the production bundle for knowledge base

--- packages/knowledge-base/gatsby-config.js
+++ packages/knowledge-base/gatsby-config.js
@@ -16,105 +16,116 @@ const {
   themeKnowledgeTypes,
 } = getThemePaths(currentTheme)
 
+const plugins = [
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'theme-assets',
+      path: themeAssetsPath,
+    },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'app-assets',
+      path: './src/assets',
+    },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'theme-knowledge-types',
+      path: themeKnowledgeTypes,
+    },
+  },
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'theme-assessments',
+      path: themeAssessmentsPath,
+    },
+  },
+  {
+    resolve: 'gatsby-plugin-react-svg',
+    options: {
+      rule: {
+        include: /assets/, // See below to configure properly
+      },
+    },
+  },
+  {
+    resolve: 'gatsby-transformer-json',
+    options: {
+      // uses the folder name to set the typename: qfqm-theme/articles => Articles
+      typeName: ({ node }) => upperFirst(path.basename(node.dir)),
+    },
+  },
+  'gatsby-plugin-sharp',
+  'gatsby-transformer-sharp',
+  {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      name: currentTheme.metadata.title,
+      short_name: currentTheme.metadata.shortName,
+      start_url: '/',
+      background_color: '#ffffff',
+      theme_color: '#663399',
+      display: 'minimal-ui',
+      icon: path.join(themeAssetsPath, 'logo.png'),
+    },
+  },
+  'gatsby-plugin-react-helmet',
+  {
+    resolve: 'gatsby-plugin-create-client-paths',
+    options: {
+      prefixes: [
+        '/admin/*',
+        '/auth/*',
+        '/my-content/*',
+        '/content/*',
+        '/profile/*',
+        '/section/*',
+        '/search/*',
+        '/submit/*',
+        '/management-report/*',
+      ],
+    },
+  },
+  {
+    resolve: 'gatsby-plugin-material-ui',
+  },
+  {
+    resolve: 'gatsby-plugin-google-fonts',
+    options: {
+      fonts: currentTheme.theme.googleFonts,
+    },
+  },
+  {
+    resolve: 'gatsby-source-graphql',
+    options: {
+      typeName: 'RawSalmon',
+      fieldName: 'raw_salmon',
+      url: process.env.GATSBY_GRAPHQL_API,
+      headers: {
+        'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+      },
+    },
+  },
+]
+
+if (process.env.ANALYZE) {
+  plugins.push({
+    resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+    options: {
+      production: true,
+    },
+  })
+}
+
 module.exports = {
   siteMetadata: {
     ...currentTheme.metadata,
     version: getApplicationVersion(version),
   },
-  plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'theme-assets',
-        path: themeAssetsPath,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'app-assets',
-        path: './src/assets',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'theme-knowledge-types',
-        path: themeKnowledgeTypes,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'theme-assessments',
-        path: themeAssessmentsPath,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: /assets/, // See below to configure properly
-        },
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-json',
-      options: {
-        //uses the folder name to set the typename: qfqm-theme/articles => Articles
-        typeName: ({ node }) => upperFirst(path.basename(node.dir)),
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: currentTheme.metadata.title,
-        short_name: currentTheme.metadata.shortName,
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: path.join(themeAssetsPath, 'logo.png'),
-      },
-    },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-create-client-paths',
-      options: {
-        prefixes: [
-          '/admin/*',
-          '/auth/*',
-          '/my-content/*',
-          '/content/*',
-          '/profile/*',
-          '/section/*',
-          '/search/*',
-          '/submit/*',
-          '/management-report/*',
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-material-ui',
-    },
-    {
-      resolve: 'gatsby-plugin-google-fonts',
-      options: {
-        fonts: currentTheme.theme.googleFonts,
-      },
-    },
-    {
-      resolve: 'gatsby-source-graphql',
-      options: {
-        typeName: 'RawSalmon',
-        fieldName: 'raw_salmon',
-        url: process.env.GATSBY_GRAPHQL_API,
-        headers: {
-          'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET,
-        },
-      },
-    },
-  ],
+  plugins,
 }
