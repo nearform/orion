75edbe291be177da4ee56b400eac789455915ccf
RudyRed <will.gross@nearform.com>
2020-01-28 22:03:33 -0500

    refactor: 💡 Move shadows to seperate file

--- packages/efqm-theme/theme/index.js
+++ packages/efqm-theme/theme/index.js
@@ -4,6 +4,7 @@ const { colorDefinitions, fontFamily } = require('./variables')
 
 const palette = require('./mui/palette')
 const typography = require('./mui/typography')
+const shadows = require('./mui/shadows')
 
 // const fontFamily = (exports.fontFamily = 'Lato, sans-serif')
 
@@ -373,14 +374,14 @@ const editorsPicks = {
 //   },
 // }
 
-const shadows = [
-  'none',
-  '0 0 5px 0 rgba(0, 0, 0, 0.05), 0 2px 5px 0 rgba(0, 0, 0, 0.05)',
-  // material ui requires  25 elevations
-  ...new Array(23).fill(
-    '0 0 5px 0 rgba(0, 0, 0, 0.1), 0 2px 10px 0 rgba(0, 0, 0, 0.1)'
-  ),
-]
+// const shadows = [
+//   'none',
+//   '0 0 5px 0 rgba(0, 0, 0, 0.05), 0 2px 5px 0 rgba(0, 0, 0, 0.05)',
+//   // material ui requires  25 elevations
+//   ...new Array(23).fill(
+//     '0 0 5px 0 rgba(0, 0, 0, 0.1), 0 2px 10px 0 rgba(0, 0, 0, 0.1)'
+//   ),
+// ]
 
 const inputClasses = {
   root: {
---
+++ packages/efqm-theme/theme/mui/shadows.js
@@ -0,0 +1,8 @@
+module.exports = [
+  'none',
+  '0 0 5px 0 rgba(0, 0, 0, 0.05), 0 2px 5px 0 rgba(0, 0, 0, 0.05)',
+  // material ui requires  25 elevations
+  ...new Array(23).fill(
+    '0 0 5px 0 rgba(0, 0, 0, 0.1), 0 2px 10px 0 rgba(0, 0, 0, 0.1)'
+  ),
+]
