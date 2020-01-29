c111de1100a97b46d2662f23aee71599804d2088
RudyRed <will.gross@nearform.com>
2020-01-28 22:00:18 -0500

    refactor: 💡 Moved typography to seperate file

--- packages/efqm-theme/theme/index.js
+++ packages/efqm-theme/theme/index.js
@@ -3,6 +3,7 @@ const { fade } = require('@material-ui/core/styles/colorManipulator')
 const { colorDefinitions, fontFamily } = require('./variables')
 
 const palette = require('./mui/palette')
+const typography = require('./mui/typography')
 
 // const fontFamily = (exports.fontFamily = 'Lato, sans-serif')
 
@@ -283,94 +284,94 @@ const editorsPicks = {
   },
 }
 
-const typography = {
-  h1: {
-    fontFamily,
-    fontWeight: '900',
-    fontSize: 28,
-    letterSpacing: 0.5,
-  },
-  h2: {
-    fontFamily,
-    fontWeight: 'bold',
-    fontSize: 18,
-    lineHeight: 'normal',
-    letterSpacing: -0.1,
-  },
-  h3: {
-    fontFamily,
-    fontWeight: '900',
-    fontSize: 12,
-    letterSpacing: 1.8,
-    textTransform: 'uppercase',
-  },
-  h4: {
-    fontFamily,
-    fontWeight: 'bold',
-    fontSize: 11,
-    letterSpacing: 1.2,
-    textTransform: 'uppercase',
-  },
-  h5: {
-    fontFamily,
-    fontWeight: 'bold',
-  },
-  h6: {
-    // Mainly used for bold numbers in tables and graphics
-    fontFamily,
-    fontSize: 20,
-    fontWeight: '900',
-    lineHeight: 1.4,
-    letterSpacing: -0.2,
-  },
-  caption: {
-    fontFamily,
-    fontSize: 14,
-    letterSpacing: 'normal',
-    color: colorDefinitions.lightGrey,
-  },
-  body2: {
-    fontFamily,
-    fontSize: 14,
-    letterSpacing: 'normal',
-  },
-  body1: {
-    fontFamily,
-    fontSize: 16,
-    letterSpacing: 'normal',
-  },
-  tableHeader: {
-    fontFamily,
-    color: colorDefinitions.slateGrey,
-    fontSize: 11,
-    fontWeight: 'bold',
-    letterSpacing: '1.2px',
-    lineHeight: '13px',
-    textTransform: 'uppercase',
-    textAlign: 'left',
-    padding: '16px 14px 10px',
-  },
-  tableCell: {
-    fontFamily,
-    fontSize: 14,
-    letterSpacing: 'normal',
-    color: colorDefinitions.slateGrey,
-    padding: '10px 14px',
-  },
-  placeholder: {
-    fontFamily,
-    fontSize: 14,
-    lineHeight: 'normal',
-    letterSpacing: -0.1,
-  },
-  button: {
-    fontFamily,
-    fontSize: 12,
-    fontWeight: '900',
-    lineHeight: 'normal',
-    letterSpacing: 1.8,
-  },
-}
+// const typography = {
+//   h1: {
+//     fontFamily,
+//     fontWeight: '900',
+//     fontSize: 28,
+//     letterSpacing: 0.5,
+//   },
+//   h2: {
+//     fontFamily,
+//     fontWeight: 'bold',
+//     fontSize: 18,
+//     lineHeight: 'normal',
+//     letterSpacing: -0.1,
+//   },
+//   h3: {
+//     fontFamily,
+//     fontWeight: '900',
+//     fontSize: 12,
+//     letterSpacing: 1.8,
+//     textTransform: 'uppercase',
+//   },
+//   h4: {
+//     fontFamily,
+//     fontWeight: 'bold',
+//     fontSize: 11,
+//     letterSpacing: 1.2,
+//     textTransform: 'uppercase',
+//   },
+//   h5: {
+//     fontFamily,
+//     fontWeight: 'bold',
+//   },
+//   h6: {
+//     // Mainly used for bold numbers in tables and graphics
+//     fontFamily,
+//     fontSize: 20,
+//     fontWeight: '900',
+//     lineHeight: 1.4,
+//     letterSpacing: -0.2,
+//   },
+//   caption: {
+//     fontFamily,
+//     fontSize: 14,
+//     letterSpacing: 'normal',
+//     color: colorDefinitions.lightGrey,
+//   },
+//   body2: {
+//     fontFamily,
+//     fontSize: 14,
+//     letterSpacing: 'normal',
+//   },
+//   body1: {
+//     fontFamily,
+//     fontSize: 16,
+//     letterSpacing: 'normal',
+//   },
+//   tableHeader: {
+//     fontFamily,
+//     color: colorDefinitions.slateGrey,
+//     fontSize: 11,
+//     fontWeight: 'bold',
+//     letterSpacing: '1.2px',
+//     lineHeight: '13px',
+//     textTransform: 'uppercase',
+//     textAlign: 'left',
+//     padding: '16px 14px 10px',
+//   },
+//   tableCell: {
+//     fontFamily,
+//     fontSize: 14,
+//     letterSpacing: 'normal',
+//     color: colorDefinitions.slateGrey,
+//     padding: '10px 14px',
+//   },
+//   placeholder: {
+//     fontFamily,
+//     fontSize: 14,
+//     lineHeight: 'normal',
+//     letterSpacing: -0.1,
+//   },
+//   button: {
+//     fontFamily,
+//     fontSize: 12,
+//     fontWeight: '900',
+//     lineHeight: 'normal',
+//     letterSpacing: 1.8,
+//   },
+// }
 
 const shadows = [
   'none',
@@ -479,11 +480,7 @@ exports.muiTheme = {
       elevation: 1,
     },
   },
-  typography: {
-    fontFamily,
-    fontWeight: 'normal',
-    ...typography,
-  },
+  typography,
   palette,
   shadows,
   iconLight,
