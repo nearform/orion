5a74fc309f563f05d01a8041efa872a6f11c79b0
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2020-01-31 10:59:51 +0000

    feat: 🎸 add initial palette colors to theme
    BREAKING CHANGE: n
    
    Closes: ch197

--- packages/gatsby-theme-acme/src/gatsby-theme-material-ui-top-layout/theme.js
+++ packages/gatsby-theme-acme/src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,5 +1,18 @@
 import { createMuiTheme } from '@material-ui/core'
 
-const theme = createMuiTheme({})
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5a49ff',
+    },
+    secondary: {
+      main: '#2aa77b',
+    },
+    text: {
+      primary: '#204c67',
+      secondary: '#fff',
+    },
+  },
+})
 
 export default theme
