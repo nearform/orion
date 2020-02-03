60211bda155231d681f5363eafb37decb7fe041c
RudyRed <will.gross@nearform.com>
2020-02-03 10:15:16 -0500

    style: 💄 Fix for xo styling

--- packages/efqm-theme/theme/mui/shadows.js
+++ packages/efqm-theme/theme/mui/shadows.js
@@ -1,7 +1,7 @@
 module.exports = [
   'none',
   '0 0 5px 0 rgba(0, 0, 0, 0.05), 0 2px 5px 0 rgba(0, 0, 0, 0.05)',
-  // material ui requires  25 elevations
+  // Material ui requires  25 elevations
   ...new Array(23).fill(
     '0 0 5px 0 rgba(0, 0, 0, 0.1), 0 2px 10px 0 rgba(0, 0, 0, 0.1)'
   ),
