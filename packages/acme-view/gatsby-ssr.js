b3d3ba175a4aab22107396bdc2f42cbd828a352f
Jack Murdoch <jack.murdoch@nearform.com>
2020-02-24 15:51:57 +0000

    chore: fixes from merge

--- packages/acme-view/gatsby-ssr.js
+++ packages/acme-view/gatsby-ssr.js
@@ -18,7 +18,7 @@ export const wrapPageElement = ({ element }) => (
         <ThemeWrapper>
           {element}
         </ThemeWrapper>
-      </ThemeProvider>>
+      </ThemeProvider>
       </ComponentProvider>
   </LayoutProvider>
 )
