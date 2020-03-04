a03e95f4e61c36d66526ac1ad780ceae2202656b
codyzu <cody.zuschlag@gmail.com>
2020-03-04 14:49:55 +0100

    feat: 🎸 add hero image to home page
    Closes: ch361

--- packages/acme-view/src/layouts/Home/index.js
+++ packages/acme-view/src/layouts/Home/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import { Grid } from '@material-ui/core'
+import heroImage from '../../../assets/orion-hero-2x.png'
+
+console.log('IMG:', heroImage)
 
 function HomeLayout({ main, page }) {
   return (
     <Layout page={page}>
+      <Grid item>
+        <img src={heroImage} />
+      </Grid>
       <h1>Home</h1>
       <div>{main}</div>
     </Layout>
