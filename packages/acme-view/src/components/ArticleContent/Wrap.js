9164185a511953db34e26f6c9d606ad2fbccea3d
Jack Murdoch <jack.murdoch@nearform.com>
2020-03-05 13:58:11 +0000

    chore: include missing files

---
+++ packages/acme-view/src/components/ArticleContent/Wrap.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import T from 'prop-types'
+import InnerArticleContent from '.'
+
+function ArticleContent({ page, image, content }) {
+  return <InnerArticleContent content={content} title={page.title} image={image} />
+}
+
+ArticleContent.propTypes = {
+  content: T.string.isRequired,
+  image: T.string,
+  page: T.object.isRequired,
+}
+
+export default ArticleContent
