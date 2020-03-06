79708e5106db170ec41abcbb4a89079e76c75767
codyzu <cody.zuschlag@gmail.com>
2020-03-06 10:22:50 +0100

    test: 💍 test background image can be set
    Closes: ch361

--- packages/acme-view/src/components/HomePageHero.test.js
+++ packages/acme-view/src/components/HomePageHero.test.js
@@ -17,3 +17,21 @@ test('renders search bar', () => {
 
   expect(searchInputEl).toBeInTheDocument()
 })
+
+test('background image can be set', () => {
+  const expectedImageSrc = 'the-image.jpg'
+  const { container } = render(
+    <ThemeProvider
+      theme={createMuiTheme({ palette: { tertiary: {}, common: {} } })}
+    >
+      <HomePageHero imageSrc={expectedImageSrc} />
+    </ThemeProvider>
+  )
+
+  // The background image should be set on the root of the grid
+  const gridRootEl = container.querySelector('.MuiGrid-root')
+
+  expect(gridRootEl).toHaveStyle({
+    backgroundImage: `url(${expectedImageSrc})`,
+  })
+})
