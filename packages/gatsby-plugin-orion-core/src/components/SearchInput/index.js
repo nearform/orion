4c4494153214f55d10415bf1fd6fe8b67c01274f
Shaun Baker <shaun@schb.co.uk>
2020-03-13 12:08:28 +0000

    feat: 🎸 419 Basic Search Functionality
    Implements basic search - minus results page and non PSQL
    
    Closes: 419

--- packages/gatsby-plugin-orion-core/src/components/SearchInput/index.js
+++ packages/gatsby-plugin-orion-core/src/components/SearchInput/index.js
@@ -1,22 +1,111 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import T from 'prop-types'
-import { Button, withStyles } from '@material-ui/core'
+import { Button, Grid, Typography, Paper, withStyles } from '@material-ui/core'
 import Search from '@material-ui/icons/Search'
+import { Link } from '@reach/router'
+import { useManualQuery } from 'graphql-hooks'
 
-function SearchInput({ classes, onSearch, placeholderText }) {
-  const [value, setValue] = useState('')
+import baseQuery from '../../queries/base-search.graphql'
+import useDebounce from '../../hooks/useDebounce'
+
+function SearchInput({
+  classes,
+  onSearch,
+  placeholderText,
+  query = baseQuery,
+}) {
+  const [state, setState] = useState({
+    term: '',
+    searched: false,
+    focused: false,
+    results: [],
+  })
+  const [queryFn, queryResult] = useManualQuery(query)
+  const debouncedSearchTerm = useDebounce(state.term, 500)
+
+  useEffect(() => {
+    if (debouncedSearchTerm && debouncedSearchTerm.length > 0) {
+      queryFn({
+        variables: {
+          term: `%${debouncedSearchTerm}%`,
+          limit: 4,
+        },
+      })
+    } else {
+      setState(s => ({
+        ...s,
+        results: [],
+      }))
+    }
+  }, [debouncedSearchTerm, queryFn])
+
+  useEffect(() => {
+    if (queryResult.data) {
+      setState(s => ({
+        ...s,
+        searched: true,
+        results: queryResult.data.results,
+      }))
+    }
+  }, [queryResult.data])
+
+  const { focused, searched, term, results } = state
+
+  const cls =
+    results.length > 0 && focused
+      ? 'results visible'
+      : focused && searched
+      ? 'no-results visible'
+      : 'results'
 
   return (
-    <div className={classes.root}>
+    <div className={focused ? `${classes.root} focused` : classes.root}>
       <input
         className={classes.input}
         placeholder={placeholderText}
-        value={value}
-        onChange={event => setValue(event.target.value)}
+        value={term}
+        onFocus={() => setState({ ...state, focused: true })}
+        onBlur={() => setState({ ...state, focused: false })}
+        onChange={event => setState({ ...state, term: event.target.value })}
       />
-      <Button className={classes.button} onClick={() => onSearch(value)}>
+      <Button className={classes.button} onClick={() => onSearch(term)}>
         <Search />
       </Button>
+      <Paper elevation={3} className={cls}>
+        <Grid container>
+          {state.results.length > 0 && (
+            <Grid item xs={12}>
+              <Typography className="results-label" variant="h5">
+                Top Hits
+              </Typography>
+            </Grid>
+          )}
+          {state.results.length > 0 &&
+            state.results.map(result => (
+              <Grid key={result.id} item xs={12}>
+                <Typography component={Link} to={result.path} variant="h5">
+                  {result.title}
+                </Typography>
+              </Grid>
+            ))}
+          {state.results && state.results.length > 0 && (
+            <Grid item xs={12}>
+              <Typography
+                component={Link}
+                to={`/search?term=${state.term}`}
+                variant="h5"
+              >
+                More results
+              </Typography>
+            </Grid>
+          )}
+          {state.results.length === 0 && (
+            <Grid item xs={12}>
+              <Typography variant="h5">No Results</Typography>
+            </Grid>
+          )}
+        </Grid>
+      </Paper>
     </div>
   )
 }
@@ -27,12 +116,14 @@ SearchInput.propTypes = {
   classes: T.object,
   onSearch: T.func,
   placeholderText: T.string,
+  query: T.string,
 }
 
 SearchInput.defaultProps = {
   placeholderText: 'Search',
   onSearch: () => undefined,
   classes: {},
+  query: undefined,
 }
 
 SearchInput.defaultProps = {
