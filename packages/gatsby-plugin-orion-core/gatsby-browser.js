0d6908902bcacf90b711b7668d0240a7d8688aff
codyzu <cody.zuschlag@gmail.com>
2020-03-17 14:52:42 +0100

    fix: 🐛 authorization persistance via cookie

--- packages/gatsby-plugin-orion-core/gatsby-browser.js
+++ packages/gatsby-plugin-orion-core/gatsby-browser.js
@@ -1,38 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import awsConfig from './utils/aws-exports'
+import React from 'react'
 import AuthWrapper from './src/components/AuthWrapper'
-import { ClientContext } from 'graphql-hooks'
-import { initGraphQLClient, makeGraphQLClient } from './utils/graphql'
-import { loadCSS } from 'fg-loadcss'
-
-const client = makeGraphQLClient(process.env.GATSBY_GRAPHQL_API)
-
-const AuthInitWrapper = ({ element }) => {
-  const [isAuthInitialized, setIsAuthInitialized] = useState(false)
-
-  useEffect(() => {
-    const init = async () => {
-      await initGraphQLClient(client, awsConfig)
-      setIsAuthInitialized(true)
-    }
-
-    init()
-  }, [])
-
-  useEffect(() => {
-    loadCSS(
-      'https://use.fontawesome.com/releases/v5.12.1/css/all.css',
-      document.querySelector('#font-awesome-css')
-    )
-  }, [])
-
-  return (
-    <ClientContext.Provider value={client}>
-      <AuthWrapper isAuthInitialized={isAuthInitialized}>{element}</AuthWrapper>
-    </ClientContext.Provider>
-  )
-}
+import GraphQLProvider from './src/components/GraphQLProvider'
 
 export const wrapRootElement = ({ element }) => (
-  <AuthInitWrapper client={client} element={element} />
+  <AuthWrapper>
+    <GraphQLProvider>{element}</GraphQLProvider>
+  </AuthWrapper>
 )
