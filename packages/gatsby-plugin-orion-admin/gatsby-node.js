5d405f3bb767b3f4d1b82b89c286ad1775bcd81c
spirift <drift.ra@gmail.com>
2020-03-23 15:58:01 +0000

    feat: 🎸 Setup orion-admin minimal runable app

--- packages/gatsby-plugin-orion-admin/gatsby-node.js
+++ packages/gatsby-plugin-orion-admin/gatsby-node.js
@@ -1,3 +1,16 @@
-exports.onPreInit = () => {
-  // Noop placeholder
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions
+
+  createPage({
+    path: '/',
+    component: require.resolve('./src/templates/home'),
+  })
+
+  createPage({
+    path: '/_admin',
+    matchPath: '/admin',
+    component: require.resolve('./src/templates/admin-page'),
+  })
+
+  return null
 }
---
+++ packages/gatsby-plugin-orion-admin/src/templates/home.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Redirect } from '@reach/router'
+
+export default function() {
+  return <Redirect to="/admin" />
+}
