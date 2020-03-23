652fb4e90fdd6c7352adba16d1e3a3d52e760aaf
Jack Murdoch <jack.murdoch@nearform.com>
2020-03-23 13:55:42 +0000

    feat: 🎸 re-ordering of pages within the existing hierarchy

--- packages/gatsby-plugin-orion-edit/src/components/PageTree/index.js
+++ packages/gatsby-plugin-orion-edit/src/components/PageTree/index.js
@@ -3,15 +3,17 @@ import TreeView from '../TreeView'
 import { mutateTree, moveItemOnTree } from '@atlaskit/tree'
 import { useEditComponents } from '../EditComponentProvider'
 import { useLocation } from '@reach/router'
-import { useQuery } from 'graphql-hooks'
+import { useMutation, useQuery } from 'graphql-hooks'
 
 import getPagesQuery from '../../queries/get-pages'
+import updatePositionMutation from '../../queries/update-position'
 
 function PageTree() {
   const { data } = useQuery(getPagesQuery)
   const { layouts } = useEditComponents()
   const { pathname } = useLocation()
   const [tree, setTree] = useState(null)
+  const [updatePosition] = useMutation(updatePositionMutation)
 
   useEffect(() => {
     const items = {
@@ -29,11 +31,22 @@ function PageTree() {
         .map(item => item.id)
 
       for (const page of data.orion_page) {
+        const sort = (a, b) => {
+          const first = data.orion_page.find(page => page.id === a)
+          const second = data.orion_page.find(page => page.id === b)
+
+          return first.position - second.position
+        }
+
+        const children = page.descendants
+          .map(({ descendant }) => descendant.id)
+          .sort(sort)
+
         items[page.id] = {
           id: page.id,
           title: page.title,
           to: `/pages/${page.id}/edit`,
-          children: page.descendants.map(({ descendant }) => descendant.id),
+          children,
           allowChildren: layouts[page.layout].allowChildren,
           iconClass:
             page.path === '/'
@@ -44,7 +57,9 @@ function PageTree() {
         }
       }
 
-      const current = data.orion_page.find(page => pathname === `/pages/${page.id}/edit`)
+      const current = data.orion_page.find(
+        page => pathname === `/pages/${page.id}/edit`
+      )
 
       if (current) {
         for (const { ancestor } of current.ancestry) {
@@ -74,8 +89,12 @@ function PageTree() {
   )
 
   const onDragEnd = useCallback(
-    (source, destination) => {
+    async (source, destination) => {
       if (!destination) {
+        return
+      }
+
+      if (source.parentId !== destination.parentId) {
         return
       }
 
@@ -86,9 +105,15 @@ function PageTree() {
         return
       }
 
-      console.log(newTree.items[newParent.id].children)
-
       setTree(newTree)
+
+      await Promise.all(
+        newTree.items[newParent.id].children.map((id, position) =>
+          updatePosition({
+            variables: { id, position },
+          })
+        )
+      )
     },
     [tree, setTree]
   )
