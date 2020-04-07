a0c8ede10e8225181a8834d397a1c52c5b95ba63
nf-richatkins <rich.atkins@nearform.com>
2020-04-07 15:15:48 +0100

    feat: 🎸 add link to view from edit
    add dynamic link from edit to view url
    
    Closes: #495

--- packages/gatsby-plugin-orion-edit/src/templates/home.js
+++ packages/gatsby-plugin-orion-edit/src/templates/home.js
@@ -38,6 +38,40 @@ const sideBarItems = [
     iconClass: 'fas fa-user-friends',
   },
   {
+    label: 'goto View',
+    to: '',
+    iconClass: 'fas fa-eye',
+    onClick: () => {
+      const hasCustomUrl =
+        process.env.GATSBY_URL_VIEW !== undefined &&
+        process.env.GATSBY_URL_VIEW.length > 0
+
+      if (hasCustomUrl) {
+        console.log('### Custom Url #' + process.env.GATSBY_URL_VIEW + '#')
+        window.location.href = process.env.GATSBY_URL_VIEW
+      } else if (!hasCustomUrl) {
+        const bit = window.location.hostname.split('.')[0]
+        switch (bit) {
+          case 'localhost':
+            console.log('### localhost')
+            window.location.href = 'http://localhost:8000'
+            break
+          case 'edit': {
+            console.log('### edit url!')
+            const { origin } = window.location
+            const idx = origin.indexOf('edit')
+            const remainder = origin.slice(idx + 4)
+            window.location.href = `${origin.slice(0, idx)}view${remainder}`
+            break
+          }
+
+          default:
+            console.error('no URL_VIEW set in environment variables')
+        }
+      }
+    },
+  },
+  {
     label: 'Sign out',
     to: '/sign-out',
     iconClass: 'fas fa-long-arrow-alt-left',
