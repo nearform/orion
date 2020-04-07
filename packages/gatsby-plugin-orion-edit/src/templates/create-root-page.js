5a726bcc7a9932f36500b94cb67d9ea322e6e9fb
spirift <drift.ra@gmail.com>
2020-04-07 09:56:47 +0100

    refactor: 💡 Add allTags to page creation templates initialState

--- packages/gatsby-plugin-orion-edit/src/templates/create-root-page.js
+++ packages/gatsby-plugin-orion-edit/src/templates/create-root-page.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import EditPage from '../components/EditPage'
+import getTagsQuery from '../queries/get-tags'
 import { navigate } from '@reach/router'
+import { useQuery } from 'graphql-hooks'
 
 export default function() {
-  const initialState = {
-    title: 'New page',
-    path: '',
-    ancestry: [],
-    authors: [
-      {
-        user: {
-          id: 1,
+  const { data, loading } = useQuery(getTagsQuery)
+
+  const initialState = useMemo(() => {
+    if (loading) {
+      return null
+    }
+
+    return {
+      title: 'New page',
+      path: '',
+      ancestry: [],
+      authors: [
+        {
+          user: {
+            id: 1,
+          },
         },
-      },
-    ],
-    contents: [],
-    descendants: [],
-    show_in_menu: true, // eslint-disable-line camelcase
+      ],
+      contents: [],
+      descendants: [],
+      show_in_menu: true, // eslint-disable-line camelcase
+
+      allTags: data.orion_tag,
+    }
+  }, [data, loading])
+
+  if (!initialState) {
+    return null
   }
 
   return (
