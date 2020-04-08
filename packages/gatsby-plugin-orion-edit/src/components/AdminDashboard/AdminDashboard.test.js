aac039b70601663e35b9f1a9591b6e0ffb2f802b
nf-richatkins <rich.atkins@nearform.com>
2020-04-08 15:53:41 +0100

    test: 💍 added tests for dynamic onclick link
    tests written for 3 conditions for link based on env variables and
    current domain
    
    Closes: #495

--- packages/gatsby-plugin-orion-edit/src/components/AdminDashboard/AdminDashboard.test.js
+++ packages/gatsby-plugin-orion-edit/src/components/AdminDashboard/AdminDashboard.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, within } from '@testing-library/react'
+import { fireEvent, render, within } from '@testing-library/react'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import theme from 'gatsby-theme-acme'
 
@@ -45,7 +45,33 @@ const mockData = [
     label: 'goto View',
     to: '',
     iconClass: 'fas fa-eye',
-    onClick: () => {},
+    onClick: () => {
+      const hasCustomUrl =
+        process.env &&
+        process.env.GATSBY_URL_VIEW !== undefined &&
+        process.env.GATSBY_URL_VIEW.length > 0
+
+      if (hasCustomUrl) {
+        window.location.href = process.env.GATSBY_URL_VIEW
+      } else if (!hasCustomUrl) {
+        const bit = window.location.hostname.split('.')[0]
+        switch (bit) {
+          case 'localhost':
+            window.location.href = 'http://localhost:8000'
+            break
+          case 'edit': {
+            const { origin } = window.location
+            const idx = origin.indexOf('edit')
+            const remainder = origin.slice(idx + 4)
+            window.location.href = `${origin.slice(0, idx)}view${remainder}`
+            break
+          }
+
+          default:
+            console.error('no URL_VIEW set in environment variables')
+        }
+      }
+    },
   },
   {
     label: 'Sign out',
@@ -75,12 +101,14 @@ const mockContent = [
     src: 'todo',
   },
 ]
+
 const renderComponent = () =>
   render(
     <ThemeProvider theme={createMuiTheme(theme)}>
       <AdminDashboard data={mockData} content={mockContent} heading="test me" />
     </ThemeProvider>
   )
+
 describe('AdminDashboard component', () => {
   it('renders a header', async () => {
     const { getByText } = renderComponent()
@@ -100,6 +128,39 @@ describe('AdminDashboard component', () => {
     expect(getByText('Users')).toBeInTheDocument()
     expect(getByText('goto View')).toBeInTheDocument()
     expect(getByText('Sign out')).toBeInTheDocument()
+  })
+
+  it('navigates to a custom url', () => {
+    delete global.window.location
+    const href = 'http://myedit.test.com'
+    window.location = new URL(href)
+    process.env.GATSBY_URL_VIEW = 'http://myview.test.com/'
+
+    const { getByText } = renderComponent()
+    fireEvent.click(getByText(/goto View/i))
+    expect(window.location.href).toEqual('http://myview.test.com/')
+  })
+
+  it('navigates to localhost if ENV blank', () => {
+    delete global.window.location
+    const href = 'http://localhost:8001'
+    window.location = new URL(href)
+    process.env.GATSBY_URL_VIEW = ''
+
+    const { getByText } = renderComponent()
+    fireEvent.click(getByText(/goto View/i))
+    expect(window.location.href).toEqual('http://localhost:8000/')
+  })
+
+  it('navigates to relative url', () => {
+    delete global.window.location
+    const href = 'http://edit.test.com'
+    window.location = new URL(href)
+    process.env.GATSBY_URL_VIEW = ''
+
+    const { getByText } = renderComponent()
+    fireEvent.click(getByText(/goto View/i))
+    expect(window.location.href).toEqual('http://view.test.com/')
   })
 
   it('renders a list of menu cards', () => {
