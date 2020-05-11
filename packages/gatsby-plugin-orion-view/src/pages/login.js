a20635caa27fc5e6a08745a49489013199bb0bf8
spirift <drift.ra@gmail.com>
2020-05-11 15:00:53 +0100

    refactor: 💡 Copy login page over

---
+++ packages/gatsby-plugin-orion-view/src/pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PaddedContainer from 'gatsby-plugin-orion-core/src/components/PaddedContainer'
+import UserAuthentication from '../components/UserAuthentication'
+
+const Login = () => (
+  <PaddedContainer>
+    <UserAuthentication />
+  </PaddedContainer>
+)
+
+export default Login
