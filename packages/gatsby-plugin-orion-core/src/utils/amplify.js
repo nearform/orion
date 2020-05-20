1e804e2d27aa2cb2ba2cf554d4111e154d4ccbbe
cepelinc <matiascepeda@gmail.com>
2020-05-20 15:21:03 -0300

    feat: 🎸 Disable Cognito integration for running locally

--- packages/gatsby-plugin-orion-core/src/utils/amplify.js
+++ packages/gatsby-plugin-orion-core/src/utils/amplify.js
@@ -1,6 +1,34 @@
 import Amplify from 'aws-amplify'
 import awsConfig from './aws-exports'
 
+const AmplifyMock = {
+  Auth: {
+    user: null,
+    currentAuthenticatedUser: () => Promise.resolve(),
+    signIn({ username, password }) {
+      if (username === 'test@test.com' && password === 'test') {
+        this.user = true
+        return Promise.resolve()
+      }
+
+      const error = new Error('whoos')
+      error.name = 'name'
+      error.code = 500
+      return Promise.reject(error)
+    },
+    signOut() {
+      this.user = null
+      return Promise.resolve()
+    },
+  },
+}
+
 Amplify.configure(awsConfig)
 
-export { Auth, Storage } from 'aws-amplify'
+const developmentEnv = process.env.NODE_ENV === 'development'
+
+const { Auth, Storage } = developmentEnv
+  ? AmplifyMock
+  : { Auth: Amplify.Auth, Storage: Amplify.Storage }
+
+export { Auth, Storage }
